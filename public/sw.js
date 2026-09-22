0193428b2d2db2b06b406eca6a76b5336aa4da1d
gokul2326 <gokulg7289@gmail.com>
2026-09-22 23:43:17 +0530

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'govprocure-cache-v2';
+const urlsToCache = [
+  '/',
+  '/manual',
+  '/js/i18n.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => {
+        if (k !== CACHE_NAME) return caches.delete(k);
+      })
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
